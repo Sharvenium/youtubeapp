b915b13055d8f882795aead876e3f91e097085be
Sharvenium <46588387+Sharvenium@users.noreply.github.com>
2020-06-21 10:37:25 +0800

    initial xcode project creation

---
+++ minimalistyoutube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  minimalistyoutube
+//
+//  Created by n sharvesh on 21/6/20.
+//  Copyright © 2020 Sharvenium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
